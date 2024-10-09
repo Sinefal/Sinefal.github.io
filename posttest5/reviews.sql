0981cca486106d608571626319cd9541973cb981
Sinefal <147592190+Sinefal@users.noreply.github.com>
2024-10-09 23:43:27 +0800

    Delete posttest5 directory

--- posttest5/reviews.sql
+++
@@ -1,9 +0,0 @@
-USE reviewsdb;
-
-CREATE TABLE reviews (
-    email VARCHAR(255) NOT NULL,
-    judul VARCHAR(255) NOT NULL,
-    rating INT NOT NULL,
-    review TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
